0ad772c052ce4cc733e2bde459958762fccec1f0
yifan-ch <117072842+yifan-ch@users.noreply.github.com>
2025-01-09 19:34:31 +0100

    feat: basic contamination

--- CookingStudent/Assets/Scripts/Contamination.cs
+++ CookingStudent/Assets/Scripts/Contamination.cs
@@ -1,16 +1,51 @@
 using UnityEngine;
 
-public class Conta : MonoBehaviour
+// using System.Collections;
+// using System.Collections.Generic;
+
+public class Contamination : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    /* public states */
+    public bool isContaminated = false;
+    public bool showContamination = true;
+    public bool isSource = false;
+
     void Start()
     {
-        
+        Contaminate(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    // void Update()
+    // {
+
+    // }
+
+    void Contaminate(GameObject obj)
     {
-        
+        if (isContaminated)
+        {
+            // set contamination
+            obj.GetComponent<Contamination>().isContaminated = true;
+
+            // update color
+            if (showContamination)
+            {
+                obj.GetComponent<Renderer>().material.color = Color.red;
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (!isContaminated)
+        {
+            return;
+        }
+
+        Contamination c = other.gameObject.GetComponent<Contamination>();
+        if (c != null && !c.isContaminated)
+        {
+            Contaminate(other.gameObject);
+        }
     }
 }
