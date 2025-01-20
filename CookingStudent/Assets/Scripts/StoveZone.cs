eee771ea3c9c1badd43f43de78646534dffe7556
wesselpierik <wessel.pierik@student.uva.nl>
2025-01-20 21:34:41 +0100

    fix: The pans now get activated by the stoves

--- CookingStudent/Assets/Scripts/StoveZone.cs
+++ CookingStudent/Assets/Scripts/StoveZone.cs
@@ -4,10 +4,15 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class StoveZone : XRSocketInteractor
 {
     protected override void OnSelectEntered(SelectEnterEventArgs args) {
-        if (args.interactable.transform.tag == "Pan") {
+        if (args.interactable.transform.CompareTag("Pan")) {
             XRBaseInteractable interactable = args.interactable;
 
             interactable.GetComponent<CustomXRGrabInteractable>().attachTransform = interactable.transform.Find("SnapTransform");
+
+            PanLogic panLogic = interactable.GetComponent<PanLogic>();
+            if (panLogic != null) {
+                panLogic.SetPan(true);
+            }
 
             base.OnSelectEntered(args);
         }
@@ -15,10 +20,15 @@ public class StoveZone : XRSocketInteractor
     }
 
     protected override void OnSelectExited(SelectExitEventArgs args) {
-        if (args.interactable.transform.tag == "Pan") {
+        if (args.interactable.transform.CompareTag("Pan")) {
             XRBaseInteractable interactable = args.interactable;
 
             interactable.GetComponent<CustomXRGrabInteractable>().attachTransform = interactable.transform.Find("GrabPoint");
+
+            PanLogic panLogic = interactable.GetComponent<PanLogic>();
+            if (panLogic != null) {
+                panLogic.SetPan(false);
+            }
 
             base.OnSelectExited(args);
         }
