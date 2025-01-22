f86c2927e5e1adf8f1f1ed8fd9d90a1d9697ea0e
gyfen <yifanchen04@gmail.com>
2025-01-22 14:26:26 +0100

    fix: fixed the fix not fixing the bug

--- CookingStudent/Assets/Scripts/DontFall.cs
+++ CookingStudent/Assets/Scripts/DontFall.cs
@@ -5,7 +5,7 @@ public class DontFall : MonoBehaviour
     void OnTriggerEnter(Collider other)
     {
         Vector3 p = other.transform.position;
-        p.y = other.bounds.center.y + 0.1f;
+        p.y = other.bounds.size.y / 2 + 0.05f;
         other.transform.position = p;
 
         Debug.Log(other);
