daf5a6ba3c2479664ef16fc8be50a99f3ba1d224
ytlidi <y_tlidi@hotmail.com>
2025-01-27 12:35:17 +0100

    feat: Running Tap works

--- CookingStudent/Assets/Scripts/WaterTapSystem.cs
+++ CookingStudent/Assets/Scripts/WaterTapSystem.cs
@@ -5,37 +5,30 @@ public class WaterTapInteraction : MonoBehaviour
 {
     public GameObject WaterStream; 
     public InputActionReference TapButton; 
-    private bool PlayerInReach = false; 
+    private bool playerInReach = false;
+    private bool activeWaterTap = false;
 
-    void Start()
-    {
-        if (WaterStream != null)
-        {
+    void Start() {
+        if (WaterStream != null) {
             WaterStream.SetActive(false);
+            TapButton.action.started += ToggleWaterStream;
         }
     }
 
-    void OnTriggerEnter(Collider other)
-    {
-        PlayerInReach = true;
+    void OnTriggerEnter(Collider other) {
+        playerInReach = true;
         TapButton.action.Enable();
     }
 
-    void OnTriggerExit(Collider other)
-    {
-        PlayerInReach = false;
+    void OnTriggerExit(Collider other) {
+        playerInReach = false;
         TapButton.action.Disable();
     }
 
-    void Update()
-    {
-        if (PlayerInReach && TapButton.action.triggered) 
-        {
-            if (WaterStream != null)
-            {
-                bool isActief = WaterStream.activeSelf;
-                WaterStream.SetActive(!isActief);
-            }
+    void ToggleWaterStream(InputAction.CallbackContext context) {
+        if (playerInReach && WaterStream != null) {
+            activeWaterTap = !activeWaterTap;
+            WaterStream.SetActive(activeWaterTap);
         }
     }
 }
