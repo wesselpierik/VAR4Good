8481ec6137ec2d6fad663fcd8e82b9ac5b856de4
wesselpierik <wessel.pierik@student.uva.nl>
2025-01-27 16:56:41 +0100

    fix: The pan now has control over the ingredients when they are in the pan, releases control on grab

--- CookingStudent/Assets/GrabShift.cs
+++ CookingStudent/Assets/GrabShift.cs
@@ -4,8 +4,11 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class CustomXRGrabInteractable : XRGrabInteractable
 {
     private bool hand = true; // RIGHT HAND
+    private bool pan = false;
 
     public override Transform GetAttachTransform(IXRInteractor interactor) {
+        if (attachTransform == null) {return base.GetAttachTransform(interactor); }
+
         if ((interactor.transform.parent.name == "Left Controller" && hand == true) || (interactor.transform.parent.name == "Right Controller" && hand == false)) {
             Vector3 newLocalPosition = attachTransform.localPosition;
             newLocalPosition.z = -newLocalPosition.z;
@@ -16,10 +19,24 @@ public class CustomXRGrabInteractable : XRGrabInteractable
         return base.GetAttachTransform(interactor);
     }
 
+    public void PanTrigger(bool value) {
+        pan = value;
+    }
+
     protected override void OnSelectExited(SelectExitEventArgs args) {
         IXRSelectInteractable obj = args.interactableObject;
         Rigidbody rb = obj.transform.GetComponent<Rigidbody>();
         rb.isKinematic = false;
+
+        Debug.Log("Removing something");
+
+        if (obj.transform.CompareTag("Ingredient") && pan) {
+            PanTrigger(false);
+            obj.transform.parent = null;
+            obj.transform.GetComponent<Collider>().excludeLayers = 0;
+
+            // obj.transform.gameObject.AddComponent<Rigidbody>();
+        }
 
         base.OnSelectExited(args);
     }
